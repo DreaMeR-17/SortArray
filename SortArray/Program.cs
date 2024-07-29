b9269a2d2410e07610ba415510577666f6308ecc
DreaMeR-17 <pronka_1993@mail.ru>
2024-07-29 11:05:03 +0600

    Добавьте файлы проекта.

---
+++ SortArray/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace SortArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            int arrayValue = 15;
+            int minValue = 1;
+            int maxValue = 50;
+            int tempNumber = 0;
+
+            int[] numbers = new int[arrayValue];
+
+            Console.WriteLine($"Дан массив размером {arrayValue} чисел.");
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = random.Next(minValue, maxValue);
+                Console.Write($"{numbers[i]} ");
+            }
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = 0; j < numbers.Length - 1; j++)
+                {
+                    if (numbers[j] > numbers[j + 1])
+                    {
+                        tempNumber = numbers[j + 1];
+                        numbers[j + 1] = numbers[j];
+                        numbers[j] = tempNumber;
+                    }
+                }
+            }
+
+            Console.WriteLine($"\n\nПредставлен упорядоченный вид массива: ");
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write($"{numbers[i]} ");
+            }
+        }
+    }
+}
